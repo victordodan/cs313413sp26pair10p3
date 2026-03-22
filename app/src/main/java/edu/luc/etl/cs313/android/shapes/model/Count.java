eb0fec85df5cef18fc0d524a9e7232a7f94961ec
‘‘ChapinGI’’ <cibendahl@luc.edu>
2026-03-22 15:20:52 -0500

    Implement Point, Outline, Polygon, StrokeColor, and Count

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Count.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Count.java
@@ -6,45 +6,47 @@ package edu.luc.etl.cs313.android.shapes.model;
  */
 public class Count implements Visitor<Integer> {
 
-    // TODO entirely your job
-
     @Override
     public Integer onPolygon(final Polygon p) {
-        return -1;
+        return 1;
     }
 
     @Override
     public Integer onCircle(final Circle c) {
-        return -1;
+        return 1;
     }
 
     @Override
     public Integer onGroup(final Group g) {
-        return -1;
+        int count = 0;
+        for (Shape s : g.getShapes()) {
+            count += s.accept(this);
+        }
+        return count;
     }
 
     @Override
     public Integer onRectangle(final Rectangle q) {
-        return -1;
+        return 1;
     }
 
     @Override
     public Integer onOutline(final Outline o) {
-        return -1;
+        return o.getShape().accept(this);
     }
 
     @Override
     public Integer onFill(final Fill c) {
-        return -1;
+        return c.getShape().accept(this);
     }
 
     @Override
     public Integer onLocation(final Location l) {
-        return -1;
+        return l.getShape().accept(this);
     }
 
     @Override
     public Integer onStrokeColor(final StrokeColor c) {
-        return -1;
+        return c.getShape().accept(this);
     }
 }
