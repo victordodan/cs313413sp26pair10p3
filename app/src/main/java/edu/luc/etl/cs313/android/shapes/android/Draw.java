b63f1f82f7930b7bfa1868a677d1cce0476e9145
victordodan <dodan_victor@yahoo.com>
2026-03-23 02:30:49 -0500

    Draw.java work in progress

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
@@ -17,8 +17,8 @@ public class Draw implements Visitor<Void> {
     private final Paint paint;
 
     public Draw(final Canvas canvas, final Paint paint) {
-        this.canvas = null; // FIXME
-        this.paint = null; // FIXME
+        this.canvas = canvas;
+        this.paint = paint;
         paint.setStyle(Style.STROKE);
     }
 
@@ -30,44 +30,71 @@ public class Draw implements Visitor<Void> {
 
     @Override
     public Void onStrokeColor(final StrokeColor c) {
+        int old_color = this.paint.getColor();
+        paint.setColor(c.getColor());
+        c.getShape().accept(this);
+        paint.setColor(old_color);
 
         return null;
     }
 
     @Override
     public Void onFill(final Fill f) {
-
+        Paint.Style old_style = paint.getStyle();
+        paint.setStyle(Paint.Style.FILL);
+        f.getShape().accept(this);
+        paint.setStyle(old_style);
         return null;
     }
 
     @Override
     public Void onGroup(final Group g) {
-
+        for (Shape s : g.getShapes()){
+            s.accept(this);
+        }
         return null;
     }
 
     @Override
     public Void onLocation(final Location l) {
-
+        canvas.save();
+        canvas.translate(l.getX(), l.getY());
+        l.getShape().accept(this);
+        canvas.restore();
         return null;
     }
 
     @Override
     public Void onRectangle(final Rectangle r) {
-
+        canvas.drawRect(0, 0, r.getWidth(), r.getHeight(), paint);
         return null;
     }
 
     @Override
     public Void onOutline(Outline o) {
-
+        Paint.Style old_style = paint.getStyle();
+        paint.setStyle(Paint.Style.STROKE);
+        o.getShape().accept(this);
+        paint.setStyle(old_style);
         return null;
     }
 
     @Override
     public Void onPolygon(final Polygon s) {
 
-        final float[] pts = null;
+        int n = s.getPoints().size();
+        if (n < 2) return null;
+
+        final float[] pts = new float[4*n];
+
+        for(int k = 0; k < n-1; k++){
+            Point p1 = s.getPoints().get(k);
+            Point p2 = s.getPoints().get((k+1)%n);
+            pts[k*4] = p1.getX();
+            pts[k*4+1] = p1.getY();
+            pts[k*4+2] = p2.getX();
+            pts[k*4+3] = p2.getY();
+        }
 
         canvas.drawLines(pts, paint);
         return null;
